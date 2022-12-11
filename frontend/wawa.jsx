616fb4f5c1339b9549be422fda8f607b67519e5a
zack alsiday <alsidayzack@gmail.com>
2022-12-11 12:02:50 -0800

    starting to implement backend

--- frontend/wawa.jsx
+++ frontend/wawa.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
-    ReactDOM.render(<h1>Welcome to WaWa Produce</h1>, root);
+    ReactDOM.render(<h1>Welcome</h1>, root);
 });
