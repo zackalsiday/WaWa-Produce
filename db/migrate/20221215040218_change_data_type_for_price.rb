53a23f099c20d29266955d966c20470e266ba95f
zack alsiday <alsidayzack@gmail.com>
2022-12-14 20:27:28 -0800

    update products price type

---
+++ db/migrate/20221215040218_change_data_type_for_price.rb
@@ -0,0 +1,6 @@
+class ChangeDataTypeForPrice < ActiveRecord::Migration[5.2]
+  def change
+    remove_column :products, :price
+    add_column :products, :price, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_12_210450) do
+ActiveRecord::Schema.define(version: 2022_12_15_040218) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "products", force: :cascade do |t|
     t.string "name", null: false
-    t.string "price", null: false
     t.integer "quantity", null: false
     t.text "description", null: false
     t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "price"
   end
 
   create_table "users", force: :cascade do |t|
