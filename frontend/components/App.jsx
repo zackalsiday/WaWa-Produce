04ab044a1f1da4e863896a02ba6b9f6043ce85fb
zack alsiday <alsidayzack@gmail.com>
2022-12-15 17:56:20 -0800

    routes are working succesfuly

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -3,9 +3,11 @@ import GreetingContainer from "./greeting/greeting_container"
 import SignUpFormContainer from './session_form/signup_form_container';
 import LogInFormContainer from './session_form/login_form_container';
 import PublicContainer from './public/public_container'
-import {Route, Switch, Link} from 'react-router-dom'
+import {Router, Switch, Link,BrowserRouter, Routes} from 'react-router-dom'
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import OrderContainer from './order/order_container'
+import Cart from './cart'
+import NavBar from "./navBar";
 const App = () => (
  <div>
     <header>
@@ -15,12 +17,19 @@ const App = () => (
       <GreetingContainer />
       <ProtectedRoute path='/' component={OrderContainer} />
     </header>
+  
+
+      {/* <AuthRoute path="/" component={NavBar}/> */}
+  
+  
     <Switch>
+      <AuthRoute   exact path="/" component={PublicContainer}/>
+      <AuthRoute exact path="/cart" component={Cart}/>
       <AuthRoute exact path="/login" component={LogInFormContainer} />
-      <AuthRoute exact path="/signup" component={SignUpFormContainer} />
-      
-      <PublicContainer/>
+      <AuthRoute  exact path="/signup" component={SignUpFormContainer} />
     </Switch>
+    
+
   </div>
 );
 export default App;
--- frontend/components/public/public.jsx
+++ frontend/components/public/public.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
-
+import { Router, Switch, Link, BrowserRouter, Route } from 'react-router-dom'
+import { AuthRoute, ProtectedRoute } from '../../util/route_util';
+import { Dropdown } from 'semantic-ui-react'
+import NavBar from '../navBar';
+import Cart from '../cart'
 class Public extends React.Component{
     constructor(props){
         super(props)
@@ -11,6 +15,40 @@ class Public extends React.Component{
      
     }
     
+    //  quantityChoice(limit){
+    //     let Options = []
+    //     for (let i = 1; i < limit; i+= 1){
+    //         Options.push(i)
+    //     }
+    //     return (
+    //         <Dropdown
+    //             placeholder='Select'
+    //             fluid
+    //             selection
+    //             options={Options}
+    //         />
+    //     )
+    // }
+    
+    async listOfOption(limit){
+        let Options = []
+            for (let i = 1; i < limit; i += 1) {
+                Options.push(i)
+            }
+            return Options
+    }
+    quantityChoice = () => (
+           
+       
+            <Dropdown
+                placeholder='Select'
+                fluid
+                selection
+                options={this.listOfOption()}
+            />
+     
+    )
+
     listProducts(){
         let list = Object.values(this.props.products)
         return (
@@ -23,7 +61,8 @@ class Public extends React.Component{
                         <h4>{item.description}</h4>
                         <h4>{item.quantity} in stock</h4>
                     </li>
-                ))}
+
+))}
             </ul>
         )
     }
@@ -33,7 +72,27 @@ class Public extends React.Component{
      
         return (
             
-           <div>{this.listProducts()}</div>
+           <div>
+            {this.listProducts()}
+                <nav>
+                    <Link to='/'>
+                        <h2>OnlineShop</h2>
+                    </Link>
+                    <Link to='/cart'>
+                        <div>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" className="bi bi-handbag-fill" viewBox="0 0 16 16">
+                                <path d="M8 1a2 2 0 0 0-2 2v2H5V3a3 3 0 1 1 6 0v2h-1V3a2 2 0 0 0-2-2zM5 5H3.36a1.5 1.5 0 0 0-1.483 1.277L.85 13.13A2.5 2.5 0 0 0 3.322 16h9.355a2.5 2.5 0 0 0 2.473-2.87l-1.028-6.853A1.5 1.5 0 0 0 12.64 5H11v1.5a.5.5 0 0 1-1 0V5H6v1.5a.5.5 0 0 1-1 0V5z" />
+                            </svg>
+                            <span>
+                                <span>
+                                    3
+                                </span>
+                            </span>
+                        </div>
+                    </Link>
+                </nav>
+            </div>
+            
         )
     }
 }
