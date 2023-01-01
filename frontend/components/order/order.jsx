ea5f9f58aa3c622ec5b7733cb88ec8e2afb965cd
zack alsiday <alsidayzack@gmail.com>
2023-01-01 12:40:33 -0800

    orderes page fully styled

--- frontend/components/order/order.jsx
+++ frontend/components/order/order.jsx
@@ -16,15 +16,22 @@ class Order extends React.Component{
         return (
             <ul>
                 {list.map((item) => (
+                <div className='order-item'>
                     <li>
                         <h4>Client: {item.name}</h4>
                         <h4>Email: {item.email}</h4>
                         <h4>Product: {item.productName}</h4>
                         {/* <h4>Product Id: {item.productId}</h4> */}
                         <h4>Quantity: {item.quantity}</h4>
-                        <h4>Total: {item.total}</h4>
+                        <h4>Total: ${item.total}</h4>
                     </li>
+                    <div className='order-options'>
+                        <button className='accept-order'>Accept</button>
+                        <button className='decline-order'>Decline</button>
+                    </div>
+                </div>
                 ))}
+                <br />
             </ul>
         )
     }
